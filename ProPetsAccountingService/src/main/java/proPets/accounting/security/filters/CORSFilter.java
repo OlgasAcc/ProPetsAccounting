cb503f2b6a29bf4d092e63517f35871b4df37f02
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-27 22:12:17 +0300

    cors

--- ProPetsAccountingService/src/main/java/proPets/accounting/security/filters/CORSFilter.java
+++ ProPetsAccountingService/src/main/java/proPets/accounting/security/filters/CORSFilter.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.annotation.Order;
@@ -17,13 +19,24 @@ import org.springframework.stereotype.Service;
 
 public class CORSFilter implements Filter {
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		response.addHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Authorization, X-Token, X-id, Content-Type");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Authorization, X-Token, X-id, Origin, Content-Type, Accept");
 		response.addHeader("Access-Control-Expose-Headers", "xsrf-token, X-Token, X-id");
-		chain.doFilter(req, res);
+		response.setStatus(200);
+		chain.doFilter(request, response);
 	}
+
 }

8fc24e1c56e5b6a4e4da10344e2fa2a5879cefad
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-27 22:39:07 +0300

    cors

--- ProPetsAccountingService/src/main/java/proPets/accounting/security/filters/CORSFilter.java
+++ ProPetsAccountingService/src/main/java/proPets/accounting/security/filters/CORSFilter.java
@@ -11,32 +11,36 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
-@Order(10)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 
 public class CORSFilter implements Filter {
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
+	@Autowired
+	FilterConfig filterConfig;
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.addHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Authorization, X-Token, X-id, Origin, Content-Type, Accept");
+		response.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+		response.addHeader("Access-Control-Max-Age", "3600");
+		response.addHeader("Access-Control-Allow-Headers",
+				"Authorization, X-Token, X-id, Origin, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
 		response.addHeader("Access-Control-Expose-Headers", "xsrf-token, X-Token, X-id");
 		response.setStatus(200);
 		chain.doFilter(request, response);
 	}
 
+	@Override
+	public void destroy() {
+	}
 }
